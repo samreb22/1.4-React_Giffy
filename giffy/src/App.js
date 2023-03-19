9a6d7ca943a5463a11f1190e00fa8c51178d7b1a
samreb22 <samuel.rebollo@dxc.com>
2023-03-19 13:32:43 +0100

    Proyecto actualizado, añadido un buscador para buscar Gifs

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'wouter';
 import './App.css';
+import GifForm from './components/GifForm';
 import ListOfGifs from './components/ListOfGifs';
 
 
@@ -8,12 +9,13 @@ function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <h1>App Giffy</h1>
+        <h1><Link to='/'>App Giffy</Link></h1>
+        <GifForm />
         <Link to='/gif/marvel'>Gifs de Marvel</Link>
         <Link to='/gif/f1'>Gifs de Fórmula 1</Link>
         <Link to='/gif/futbol'>Gifs de Fútbol</Link>
         <Link to='/gif/messi'>Gifs de Messi</Link>
-        <Route 
+        <Route
           path='/gif/:keyword' 
           component={ListOfGifs}>
         </Route>
---
+++ giffy/src/components/GifForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import './GifForm.css'
+
+function GifForm() {
+  const [gif, setGif] = useState("");
+
+  const handleInputChange = (event) => {
+    setGif(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.location.href = `/gif/${gif}`;
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Buscar GIFs:
+        <input type="text" placeholder="Introduce texto..." value={gif} onChange={handleInputChange} />
+      </label>
+      <button type="submit">Buscar</button>
+    </form>
+  );
+}
+
+export default GifForm;
